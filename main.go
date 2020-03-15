6255f927e8f2de90de41244f9bc3ebe4367509ab
Domen Skamlic <domen@skamlic.com>
2020-03-15 01:22:05 +0100

    add basic boilerplate code for serving a website

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-package wiki
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
