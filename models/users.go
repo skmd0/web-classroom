52a47ebb4821a024f0d9aeeb35c6fccfa3dbaf80
Domen Skamlic <domen@skamlic.com>
2020-03-29 14:32:34 +0200

    add Password and PasswordHash to User struct

--- models/users.go
+++ models/users.go
@@ -95,6 +95,8 @@ func (us *UserService) AutoMigrate() error {
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null; unique_index"`
+	Name         string
+	Email        string `gorm:"not null; unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
