41357bd673f75c59bd1c5494e275ade1c01f0c76
Domen Skamlic <domen@skamlic.com>
2020-03-31 17:14:41 +0200

    add random byte slice generator helper function

---
+++ rand/strings.go
@@ -0,0 +1,13 @@
+package rand
+
+import "crypto/rand"
+
+// Bytes generates n random bytes
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
