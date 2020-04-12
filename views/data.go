03f5e566485127c41c09f100dc880259d60427dc
Domen Skamlic <domen@skamlic.com>
2020-04-12 13:15:07 +0200

    add generic message for notification errors

--- views/data.go
+++ views/data.go
@@ -5,6 +5,9 @@ const (
 	AlertLvlWarning = "is-warning"
 	AlertLvlInfo    = "is-info"
 	AlertLvlSuccess = "is-success"
+
+	// AlertMsgGeneric is displayed when any unpredictable error is encountered by our backend
+	AlertMsgGeneric = "Something went wrong. Please try again, and contact us if the problem persists."
 )
 
 // Alert is data used to render Bulma notifications
