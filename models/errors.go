16db5286d43a4e06b03488a526de3e0bfa9eefc5
Domen Skamlic <domen@skamlic.com>
2020-04-13 16:37:06 +0200

    add private model errors

--- models/errors.go
+++ models/errors.go
@@ -5,14 +5,13 @@ import (
 )
 
 const (
+	// USER ERRORS
+
 	// ErrNotFound is returned when a resource cannot be found in the database.
 	ErrNotFound modelError = "models: resource not found"
 
 	// ErrPasswordInvalid is returned when an invalid password is provided
 	ErrPasswordInvalid modelError = "models: provided password is invalid"
-
-	// ErrIdInvalid is returned when an invalid ID is provided to a method like Delete(
-	ErrIdInvalid modelError = "models: ID provided was invalid"
 
 	// ErrEmailRequire is returned when there is not email set
 	ErrEmailRequired modelError = "models: email address is required"
@@ -33,11 +32,25 @@ const (
 	ErrPasswordHashRequired modelError = "models: password hash is required"
 
 	// ErrRememberTooShort is returned when remember token is too short
-	ErrRememberTooShort modelError = "models: remember token must be at least 32 bytes"
+	ErrRememberTooShort privateError = "models: remember token must be at least 32 bytes"
 
 	// ErrRememberRequired is returned when a Create() or Update() is attempted
 	// without a user remember token hash
-	ErrRememberRequired modelError = "models: remember token hash is required"
+	ErrRememberRequired privateError = "models: remember token hash is required"
+
+	// ErrIdInvalid is returned when an invalid ID is provided to a method like Delete(
+	ErrIdInvalid privateError = "models: ID provided was invalid"
+
+	// POST ERRORS
+
+	// ErrUserIdRequired is returned when userID is 0 or less
+	ErrUserIdRequired privateError = "models: post UserID is invalid"
+
+	// ErrTitleRequired is returned when post without title is submitted
+	ErrTitleRequired modelError = "models: post title is required"
+
+	// ErrContentRequired is returned when post without content is submitted
+	ErrContentRequired modelError = "models: post content is required"
 )
 
 type modelError string
@@ -49,4 +62,10 @@ func (e modelError) Error() string {
 func (e modelError) Public() string {
 	s := strings.Replace(string(e), "models: ", "", 1)
 	return strings.Title(s)
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
