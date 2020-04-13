9f58eeef4fd74d02d1ae56f837fcd81cc1410dcb
Domen Skamlic <domen@skamlic.com>
2020-04-13 16:37:53 +0200

    add post validators for Create()

--- models/posts/post_validators.go
+++ models/posts/post_validators.go
@@ -1,5 +1,7 @@
 package posts
 
+import "wiki/models"
+
 func newPostValidator(pdb PostDB) *postValidator {
 	return &postValidator{
 		PostDB: pdb,
@@ -8,4 +10,49 @@ func newPostValidator(pdb PostDB) *postValidator {
 
 type postValidator struct {
 	PostDB
+}
+
+// validator function type signature
+type postValFunc func(*Post) error
+
+// runPostValFunc loops through all the validator and returns err if any fail
+func runPostValFunc(post *Post, fns ...postValFunc) error {
+	for _, fn := range fns {
+		if err := fn(post); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (pv *postValidator) Create(post *Post) error {
+	err := runPostValFunc(post,
+		pv.titleRequired,
+		pv.contentRequired,
+		pv.userIDRequired)
+	if err != nil {
+		return err
+	}
+	return pv.PostDB.Create(post)
+}
+
+func (pv *postValidator) userIDRequired(post *Post) error {
+	if post.UserID <= 0 {
+		return models.ErrUserIdRequired
+	}
+	return nil
+}
+
+func (pv *postValidator) titleRequired(post *Post) error {
+	if post.Title == "" {
+		return models.ErrTitleRequired
+	}
+	return nil
+}
+
+func (pv *postValidator) contentRequired(post *Post) error {
+	if post.Content == "" {
+		return models.ErrContentRequired
+	}
+	return nil
 }
